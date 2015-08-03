66ac39ec30ce1940b765dbe6870bfa862e5c7eaa
Storycoder <joseph.margolis@gmail.com>
2015-08-03 15:14:53 -0400

    testing and eRB

--- test/controllers/joe_stock_controller_test.rb
+++ test/controllers/joe_stock_controller_test.rb
@@ -4,16 +4,19 @@ class JoeStockControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | joeSTOCK.com"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | joeSTOCK.com"
   end
 
   test "should get contact" do
   	get :contact
   	assert_response :success
+    assert_select "title", "Contact | joeSTOCK.com"
   end
 
 end
