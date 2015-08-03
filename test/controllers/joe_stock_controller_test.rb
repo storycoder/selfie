102032f36869946af8b64a07da6eb944fe7678f6
Storycoder <joseph.margolis@gmail.com>
2015-08-03 15:21:48 -0400

    Eliminate repetition (solves exercise 3.1)

--- test/controllers/joe_stock_controller_test.rb
+++ test/controllers/joe_stock_controller_test.rb
@@ -1,22 +1,26 @@
 require 'test_helper'
 
 class JoeStockControllerTest < ActionController::TestCase
+  def setup
+    @base_title = "joeSTOCK.com"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | joeSTOCK.com"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | joeSTOCK.com"
+    assert_select "title", "About | #{@base_title}"
   end
 
   test "should get contact" do
   	get :contact
   	assert_response :success
-    assert_select "title", "Contact | joeSTOCK.com"
+    assert_select "title", "Contact | #{@base_title}"
   end
 
 end
